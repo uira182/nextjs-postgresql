a753393c958707213fc370c7e3dd6db34351fd38
Uirá de Souza <uira182@hotmail.com>
2022-08-13 01:30:00 -0300

    Criação do insert no banco e retorno do id inserido.

--- src/pages/api/tasks/index.tsx
+++ src/pages/api/tasks/index.tsx
@@ -1,13 +1,26 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from 'next'
+import { conn } from '../../../utils/database'
 
-export default function index(req: NextApiRequest, res: NextApiResponse){
+export default async(req: NextApiRequest, res: NextApiResponse) => {
    
-    const { method } = req;
+    const { method, body } = req;
 
     switch (method) {
         case "GET":
             return res.status(200).json('getting tasks');
         case "POST":
+            
+            const { title, description } = body;
+
+            const query = "INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *";
+
+            const values = [title, description];
+
+            const response = await conn.query(query, values);
+
+            console.log(response);
+
             return res.status(201).json('creating tasks');
         default:
             return res.status(400).json("Invalid method");
