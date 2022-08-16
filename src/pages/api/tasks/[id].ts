534ea29448551ef44b65ce2e44b74c884a7f51bb
Uirá de Souza <uira182@hotmail.com>
2022-08-16 20:43:46 -0300

    Criação do PUT e DELETE.

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,17 +1,53 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { conn } from '../../../utils/database'
 
-export default function index(req: NextApiRequest, res: NextApiResponse){
+export default async function index(req: NextApiRequest, res: NextApiResponse){
    
-    const { method } = req;
+    const { method, query, body } = req;
 
     switch (method) {
         case "GET":
-            return res.status(200).json('getting unique task');
+            try {
+                const result = await conn.query("SELECT * FROM tasks WHERE id = $1 RETURNING *", [query.id])
+                console.log(result['rows'])
+                if (result['rows'].length > 0){
+                    return res.status(200).json(result['rows']);
+                }else{
+                    return res.status(500).json({message: 'task not found'})
+                }
+            } catch (error: any) {
+                res.status(500).json({message: error.message})
+            }
         case "PUT":
-            return res.status(201).json('updating a unique task');
+            try {
+                const {title, description} = body;
+                const values = [title, description, query.id]
+                const result = await conn.query("UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *", values)
+                console.log(result['rows'])
+                if (result['rows'].length > 0){
+                    return res.status(200).json(result['rows']);
+                }else{
+                    return res.status(500).json({message: 'task not found'})
+                }
+            } catch (error: any) {
+                res.status(500).json({message: error.message})
+            }
         case "DELETE":
-            return res.status(201).json('deleting a unique task');
+            try {
+                const result = await conn.query("DELETE FROM tasks WHERE id = $1 RETURNING *", [query.id])
+
+                console.log(result)
+
+                if (result.rowCount > 0){
+                    return res.status(200).json({message: 'DELETE SUCCESS'})
+                }else{
+                    return res.status(500).json({message: 'ERROR, NOT DELETE'})
+                }
+
+            } catch (error: any) {
+                res.status(500).json({message: error.message})
+            }
         default:
-            return res.status(400).json("method not allowed");
+            return res.status(400).json({message: "method not allowed"});
     }
 }
